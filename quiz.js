21ecbf7bd0153059a66e638ec77ab44d440a7f39
Umi130 <o.cornevin@gmail.com>
2017-11-28 16:16:39 +0100

    Changing Subject

--- quiz.js
+++ quiz.js
@@ -1,39 +1,34 @@
 var quiz = [
   {
-    question: "How many contaminant contains a cigarette butt?",
-    choices: ["40", "400", "4000"],
+    question: "What is the third main contaminant in the ocean?",
+    choices: ["Plastic", "Oil", "Cigarette butt"],
     answer: 2,
-    image: "./q1.gif",
-    success_message: "Indeed ! Cigarette butt contain toxic chemicals such as arsenic, heavy metals and nicotine.",
-    failed_message: "WRONG ! But it's hard to believe that cigarette but contain as much as 4000 contaminant. You can find toxic chemicals such as arsenic, heavy metals and nicotine among them."
+    success_message: "",
+    failed_message: ""
   },
   {
-    question: "What is the country that is considered to be most respectful toward the environment",
-    answer: "Finland",
-    image: "Finland.jpg",
-    success_message: "Y",
-    failed_message: "N"
+    question: "",
+    answer: "",
+    success_message: "",
+    failed_message: ""
   },
   {
-    question: "Quelle librairie CSS me permet d'utiliser une bibliothèque de styles ?",
-    choices: ["bootstrap", "jquery", "vueJS"],
-    answer: 0,
-    image: "hello",
-    success_message: "Y",
-    failed_message: "N"
-  },
-  {
-    question: "Laquelle de ces expressions Javascript est valide ?",
-    choices: ["console.log(Regarde dans ta console)", "console:log('Regarde dans ta console')", "console.log('Regarde dans ta console')"],
+    question: "The accumulation of plastic in the ocean is so overwhelming that it has produced a seventh continent? What is its size?",
+    choices: ["1/6th the size of France", "the size of France", "6 times the size of France"],
     answer: 2,
-    image: "hello",
+    success_message:"Indeed that represents 3,5 square kilometers",
+    failed_message: "N"
+  },
+  {
+    question: "",
+    choices: [""],
+    answer: 2,
     success_message: "Y",
     failed_message: "N"
   },
   {
-    question: "Hello",
-    answer: "21",
-    image: "hello",
+    question: "",
+    answer: "",
     success_message: "Y",
     failed_message: "N"
   }
