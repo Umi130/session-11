e88b77518cc045b47593cd89ba08877f89f46841
Umi130 <o.cornevin@gmail.com>
2017-12-04 19:47:27 +0900

    Questions Updated Next Step is Design

--- quiz.js
+++ quiz.js
@@ -1,16 +1,23 @@
 var quiz = [
   {
-    question: "What is the third main contaminant in the ocean?",
-    choices: ["Plastic", "Oil", "Cigarette butt"],
-    answer: 2,
+    question: "What is the most common contaminant found in the ocean?",
+    answer: "Plastic",
     success_message: "",
     failed_message: ""
   },
   {
-    question: "",
-    answer: "",
+    question: "By what year will plastic outnumber the fish in the ocean",
+    choices: ["2025","2050", "2075", "Never"],
+    answer: 1,
     success_message: "",
     failed_message: ""
+  },
+  {
+    question: "How many percent of seabirds eat plastic on a regular basis ?",
+    choices: ["10%", "50%", "90%"]
+    answer: 2,
+    success_message: "Yes, the numbers are stagerring. ",
+    failed_message: "N"
   },
   {
     question: "The accumulation of plastic in the ocean is so overwhelming that it has produced a seventh continent? What is its size?",
@@ -19,17 +26,41 @@ var quiz = [
     success_message:"Indeed that represents 3,5 square kilometers",
     failed_message: "N"
   },
+    {
+    question: "In which year was this Great Pacific Garbage Patch found",
+    answer: "1997",
+    success_message: "Y",
+    failed_message: "N"
+  }
+   {
+    question: "For how many per cent does land-based pollution account for marine pollution",
+    choices: ["50%", "80%", "100%"],
+    answer: 1,
+    success_message: "Y",
+    failed_message: "N"
+  },  
   {
-    question: "",
-    choices: [""],
-    answer: 2,
+    question: "Boyan Slat is the founder of the project Ocean Cleanup. What is his project about?",
+    answer: ["Install floating traps to recover plastic in the ocean", "Commercialise a new kind of plastic that also serve as food for marine species", "Invent a liquid solution that will decompose plastic in safe bits"]
     success_message: "Y",
     failed_message: "N"
   },
   {
-    question: "",
-    answer: "",
+    question: "What is the most commonly recovered item in the annual International Cleanup event?",
+    answer: "Cigarette Butt",
     success_message: "Y",
     failed_message: "N"
+  },
+    {
+    question: "How much liter of water can one cigarette butt pollute if thrown into",
+    answer: ["50 litres", "500 litres", "5000 liters"]
+    success_message: "Y",
+    failed_message: "N"
+  }
+  {
+    question: "In which month the next ocean clean up day takes place?",
+    answer: "September",
+    success_message: "Exactly ! It takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate?",
+    failed_message: "Actually, It takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate ?"
   }
 ];
