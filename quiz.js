2f97110438b695e04fa1a5bda7c41ef767237a78
Océane CORNEVIN <13407@PST6874.sciences-po.fr>
2017-12-06 19:41:37 +0100

    Faute corrigée

--- quiz.js
+++ quiz.js
@@ -27,31 +27,31 @@ var quiz = [
     choices: ["1/6th", "the same size", "6 times more"],
     answer: 2,
     success_message:"Indeed, that represents 3,5 million square kilometers. But in reality this plastic continent is made of much plastic, most of it is not visible because it is caught underwater. If we were to take it out of the water, it would cover the surface of all the oceans.",
-    failed_message: "It's 6 times more the size France, in other words 3,5 million square kilometers. And this is only the surface... If we were to take out of the water all the plastic that are being kept under the surface, it would be able to cover the surface of all the oceans.",
+    failed_message: "It's 6 times more than the size France, in other words 3,5 million square kilometers. And this is only the surface... If we were to take out of the water all the plastic that are being kept under the surface, its dimension would be able to cover the surface of all the oceans.",
     img:"./Tires.jpg"
   },
     {
     question: "Where is located the biggest plastic continent?",
     choices: ["In the Pacific Ocean", "In the Atlantic Ocean", "In the Indian Ocean"],
     answer: 0,
-    success_message: "That is right, the biggest plastic continent also called the Great Pacific Garbage Patch is located in the Pacific oOcean between Hawai and the californian coast.",
-    failed_message: "The biggest plastic continent also called the Great Pacific Garbage Patch is located in the Pacific ocean between Hawai and the Californian coast.",
+    success_message: "That is right, the biggest plastic continent, also called the Great Pacific Garbage Patch, is located in the Pacific Ocean between Hawai and the Californian coast.",
+    failed_message: "The biggest plastic continent, also called the Great Pacific Garbage Patch, is located in the Pacific Ocean between Hawai and the Californian coast.",
     img:"./PlasticFinalDestination.gif"
   },
    {
     question: "For how many per cent does land-based pollution account for marine pollution",
     choices: ["50%", "80%", "100%"],
     answer: 1,
-    success_message: "You are right! 80% of marine pollution originates from land. The rubbish that ends up in the sea are either carried by the wind, the rivers or carelessly thrown by humans.",
-    failed_message: "Right now, it's 80% of it that originates from land-based pollution. The rubbish that ends up in the sea are either carried by the wind, the rivers or carelessly thrown by humans.",
+    success_message: "You are right! 80% of marine pollution originates from land. The rubbish that ends up in the sea are either carried by the wind, the rivers or carelessly thrown in by humans.",
+    failed_message: "Right now, it's 80% of marine poullution that originates from land. The rubbish that ends up in the sea are either carried by the wind, the rivers or carelessly thrown in by humans.",
     img:"./Cleanup.jpg"
   },  
   {
     question: "Boyan Slat is the founder of the project Ocean Cleanup. What is his project about?",
     choices: ["Install floating traps to recover plastic in the ocean", "Commercialise a new kind of plastic on which marine species can safely feed on", "Invent a liquid solution that will decompose plastic in safe bits"],
     answer: 0,
-    success_message: "That's correct, Boyan plans actually to launch its first mission in the coming months. He hopes to clean 50% of the Great Pacific Gargage Patch within 5 years.",
-    failed_message: "The correct answer is to install floating traps to recover plastic in the ocean. He hopes to clean 50% of the Great Pacific Gargage Patch within 5 years.",
+    success_message: "That's correct, Boyan actually plans to launch its first mission in the coming months. He hopes to clean 50% of the Great Pacific Gargage Patch within 5 years.",
+    failed_message: "The correct answer is to install floating traps to recover plastic in the ocean. Bryan hopes to clean 50% of the Great Pacific Gargage Patch within 5 years.",
     img:"./Slat.gif"
   },
   {
@@ -65,15 +65,15 @@ var quiz = [
     question: "How much liter of water can one cigarette butt pollute if thrown into?",
     choices: ["50 litres", "500 litres", "5000 liters"],
     answer: 2,
-    success_message: "You are right. It's 5000 liters of water contaminated for one cigarette butt.",
-    failed_message: "The correct answer is 5000 liters.",
+    success_message: "You are right. Around 5000 liters of water get contaminated by just one cigarette butt.",
+    failed_message: "The correct answer is 5000 liters!",
     img:"./Seagullsomking.jpg"
   },
   {
     question: "In which month the next ocean clean up day takes place?",
     answer: "September",
     success_message: "Exactly ! It takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate?",
-    failed_message: "Actually, It takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate ?",
+    failed_message: "Actually, it takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate ?",
     img:"./Turtle.gif"
   }
 ];
