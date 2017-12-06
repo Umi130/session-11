83f1751099ed6a9da6af616e480337c46d4a0ef1
Océane CORNEVIN <13407@PST6874.sciences-po.fr>
2017-12-06 19:18:23 +0100

    Add-in images

--- quiz.js
+++ quiz.js
@@ -1,69 +1,79 @@
 var quiz = [
   {
-    question: "What is the most common contaminant found in the ocean?",
+    question: "What is the most common contaminant found in the ocean? (1 word)",
     answer: "plastic",
     success_message: "Indeed, 8 million tonnes of plastic end up in the sea every year.",
-    failed_message: "It's plastic! 8 million tonnes of plastic end up in the sea every year."
+    failed_message: "It's plastic! 8 million tonnes of plastic end up in the sea every year.",
+    img:"./Plasticbeach.jpg"
   },
   {
     question: "By what year will plastic outnumber the fish in the ocean",
     choices: ["2025","2050", "2075", "Never"],
     answer: 1,
     success_message: "That is correct. The Ellen MacArthur Foundation reported at the World Economic Forum in 2016 that, if business goes as usual, the ocean will contain 1 tonne of plastic for every 3 tonnes of fish by 2025 and more plastic than fish by 2050.",
-    failed_message: "The correct answer is 2050. The Ellen MacArthur Foundation reported at the World Economic Forum in 2016 that if business goes usual, the ocean will contain 1 tonnes of plastic for every tonnes of fish by 2025 and more plastic than fish by 2050."
+    failed_message: "The correct answer is 2050. The Ellen MacArthur Foundation reported at the World Economic Forum in 2016 that if business goes usual, the ocean will contain 1 tonnes of plastic for every tonnes of fish by 2025 and more plastic than fish by 2050.",
+    img:"./PlasticOcean.jpg"
   },
   {
     question: "How many percent of seabirds eat plastic on a regular basis ?",
     choices: ["10%", "50%", "90%"],
     answer: 2,
     success_message: "Yes, that percentage is stagerring. At the current rate, almost 100% of seabirds will have tasted plastic by 2050.",
-    failed_message: "Actually, it is 90%. That percentage is stagerring but if we keep going at the same rate, this number will get worse : by 2050, almost 100% of seabirds will have eaten plastic."
+    failed_message: "Actually, it is 90%. That percentage is stagerring but if we keep going at the same rate, this number will get worse : by 2050, almost 100% of seabirds will have eaten plastic.",
+    img:"./Seabird.jpg"
   },
   {
     question: "The accumulation of plastic in the ocean is so overwhelming that it has produced a seventh continent? What is the size of its surface compared to the size of France?",
     choices: ["1/6th", "the same size", "6 times more"],
     answer: 2,
     success_message:"Indeed, that represents 3,5 million square kilometers. But in reality this plastic continent is made of much plastic, most of it is not visible because it is caught underwater. If we were to take it out of the water, it would cover the surface of all the oceans.",
-    failed_message: "It's 6 times more the size France, in other words 3,5 million square kilometers. And this is only the surface... If we were to take out of the water all the plastic that are being kept under the surface, it would be able to cover the surface of all the oceans."
+    failed_message: "It's 6 times more the size France, in other words 3,5 million square kilometers. And this is only the surface... If we were to take out of the water all the plastic that are being kept under the surface, it would be able to cover the surface of all the oceans.",
+    img:"./Tires.jpg"
   },
     {
     question: "Where is located the biggest plastic continent?",
     choices: ["In the Pacific Ocean", "In the Atlantic Ocean", "In the Indian Ocean"],
     answer: 0,
     success_message: "That is right, the biggest plastic continent also called the Great Pacific Garbage Patch is located in the Pacific oOcean between Hawai and the californian coast.",
-    failed_message: "The biggest plastic continent also called the Great Pacific Garbage Patch is located in the Pacific ocean between Hawai and the Californian coast."
+    failed_message: "The biggest plastic continent also called the Great Pacific Garbage Patch is located in the Pacific ocean between Hawai and the Californian coast.",
+    img:"./PlasticFinalDestination.gif"
   },
    {
     question: "For how many per cent does land-based pollution account for marine pollution",
     choices: ["50%", "80%", "100%"],
     answer: 1,
     success_message: "You are right! 80% of marine pollution originates from land. The rubbish that ends up in the sea are either carried by the wind, the rivers or carelessly thrown by humans.",
-    failed_message: "Right now, it's 80% of it that originates from land-based pollution. The rubbish that ends up in the sea are either carried by the wind, the rivers or carelessly thrown by humans."
+    failed_message: "Right now, it's 80% of it that originates from land-based pollution. The rubbish that ends up in the sea are either carried by the wind, the rivers or carelessly thrown by humans.",
+    img:"./Cleanup.jpg"
   },  
   {
     question: "Boyan Slat is the founder of the project Ocean Cleanup. What is his project about?",
     choices: ["Install floating traps to recover plastic in the ocean", "Commercialise a new kind of plastic on which marine species can safely feed on", "Invent a liquid solution that will decompose plastic in safe bits"],
     answer: 0,
     success_message: "That's correct, Boyan plans actually to launch its first mission in the coming months. He hopes to clean 50% of the Great Pacific Gargage Patch within 5 years.",
-    failed_message: "The correct answer is to install floating traps to recover plastic in the ocean. He hopes to clean 50% of the Great Pacific Gargage Patch within 5 years."
+    failed_message: "The correct answer is to install floating traps to recover plastic in the ocean. He hopes to clean 50% of the Great Pacific Gargage Patch within 5 years.",
+    img:"./Slat.gif"
   },
   {
-    question: "What is the most commonly recovered item in the annual International Cleanup event?",
+    question: "What is the most commonly recovered item in the annual International Cleanup event? (2 words)",
     answer: "cigarette butt",
-    success_message: "That is right, cigarette butt were the most recovered item from the beaches",
-    failed_message: "It's actually cigarette butt. They were the most recovered item from the beaches."
+    success_message: "That is right, cigarette butt were the most recovered item from the beaches in 2017.",
+    failed_message: "It's actually cigarette butt. They were the most recovered item from the beaches in 2017.",
+    img:"./q1.gif"
   },
     {
     question: "How much liter of water can one cigarette butt pollute if thrown into?",
     choices: ["50 litres", "500 litres", "5000 liters"],
     answer: 2,
     success_message: "You are right. It's 5000 liters of water contaminated for one cigarette butt.",
-    failed_message: "The correct answer is 5000 liters."
+    failed_message: "The correct answer is 5000 liters.",
+    img:"./Seagullsomking.jpg"
   },
   {
     question: "In which month the next ocean clean up day takes place?",
     answer: "September",
     success_message: "Exactly ! It takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate?",
-    failed_message: "Actually, It takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate ?"
+    failed_message: "Actually, It takes places every year on the third saturday in September. So for 2018, it will happen on the 15th. Will you participate ?",
+    img:"./Turtle.gif"
   }
 ];
